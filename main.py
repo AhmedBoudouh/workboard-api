fdd4abe7039ffda1a3f46204e744ca8a8c253db7
Ahmedbooudouh <boud0205@algonquinlive.com>
2026-09-15 22:12:21 -0400

    feat:implement WB-002 partial update and delete tasks

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from pydantic import BaseModel, Field
 from enum import Enum 
 import itertools
 from datetime import UTC, datetime, timezone
+from typing import Annotated
 
 app=FastAPI()
 tasks_db=[]
@@ -13,12 +14,13 @@ tasks_db=[]
 _id_counter= itertools.count(1)
 def next_id()-> int:
     return next(_id_counter)
-class Status_var(str,Enum):
+
+class StatusVar(str,Enum):
     todo = "todo"
     in_progress = "in_progress"
     done = "done"
     
-class Priority_var(str,Enum):
+class PriorityVar(str,Enum):
     low = "low"
     medium = "medium"
     high = "high"
@@ -29,14 +31,28 @@ class Priority_var(str,Enum):
 class TasksIn(BaseModel):
     title: str = Field(min_length =3, max_length=100)
     description: str| None = Field(default=None, max_length=500 )
-    priority: Priority_var =Field(default=Priority_var.medium)
+    priority: PriorityVar =Field(default=PriorityVar.medium)
     
 
 class TasksOut(TasksIn):    
     id: int
-    status: Status_var =Field(default=Status_var.todo)
+    status: StatusVar =Field(default=StatusVar.todo)
     created_at: datetime
     
+class UpdateData(BaseModel):
+        
+        title: str  = Field(default=None, min_length =3, max_length=100)
+        description: str| None = Field(default=None, max_length=500 )
+        priority: PriorityVar | None =None
+        status: StatusVar |None =None  
+        
+def extract_task (item) -> TasksOut| None:
+    for task in tasks_db:
+            if task.id == item:
+              return task       
+        
+
+
  
     
 @app.get("/health", status_code=status.HTTP_200_OK)   
@@ -59,9 +75,56 @@ async def get_tasks()->list[TasksOut]:
 
 @app.get("/tasks/{task_id}",status_code=status.HTTP_200_OK)
 async def get_tasks_by_id(task_id:int):
-    for t in tasks_db:
-        if t.id == task_id:
-          return t
+      task =extract_task (task_id)
+      if not task:
+          raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="task mot found")
+      return task
       
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="task mot found")
     
+    
+    
+    ##WB-002 — Update and Delete Tasks
+@app.patch("/tasks/{task_id}", status_code=status.HTTP_200_OK) 
+async def update_tasks(task_id:int,data:UpdateData ):
+    task = extract_task (task_id)
+    if not task:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="task mot found")
+   
+    stored_data= task.model_dump()
+    updated_data = data.model_dump(exclude_unset=True)
+    
+    for key in updated_data:
+        # if key =="title" and updated_data['title'] is None:     don't need it the class UpdateData did it (pydantic validation)
+        #     raise HTTPException(status_code=404,detail="titles couldn't be null")
+            
+        if updated_data[key] != stored_data[key]:
+             setattr(task,key,updated_data[key])
+    return task
+        
+  
+    # if data.title :
+    #   task.title = data.title
+    # if data.description==  "":
+    #         task.description= ""
+    # if data.description:
+    #     task.description= data.description
+        
+    # if data.priority:       
+    #     task.priority = data.priority
+    # if data.status:    
+    #     task.status =data.status
+    
+    # return task
+
+@app.delete("/tasks/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_task(task_id:int):
+    task = extract_task(task_id) 
+    if not task:
+         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="No content")
+    tasks_db.remove(task)
+   
+    
+
+    
+
+
