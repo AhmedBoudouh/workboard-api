48efbb3c48e424a3d8bd4f2f2b746cf83fcfb86a
Ahmedbooudouh <boud0205@algonquinlive.com>
2026-09-22 21:31:01 -0400

    feat: add database persistence with SQLAlchemy

--- main.py
+++ main.py
@@ -9,32 +9,38 @@
 import math
 
 from fastapi import FastAPI, HTTPException, status, Query, Depends
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 from enum import Enum
-import itertools
-from datetime import UTC, datetime, timezone, timedelta
+from datetime import datetime, timezone, timedelta
 from typing import Annotated
 from pwdlib import PasswordHash
 import jwt
 from jwt.exceptions import InvalidTokenError
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped, sessionmaker, Session
+from collections.abc import Generator
 
 app = FastAPI()
-tasks_db = []
 SECRET_KEY = "my-super-secret-key"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+DATABASE_URL = "sqlite:///./workboard.db"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
+
 
 password_hash = PasswordHash.recommended()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-users_db = {}
 
-_id_counter = itertools.count(1)
-
-
-def next_id() -> int:
-    return next(_id_counter)
+def get_db() -> Generator[Session, None, None]:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 class StatusVar(str, Enum):
@@ -72,6 +78,7 @@ class TasksOut(TasksIn):
     id: int
     status: StatusVar = Field(default=StatusVar.todo)
     created_at: datetime
+    model_config = ConfigDict(from_attributes=True)
 
 
 class UpdateData(BaseModel):
@@ -106,45 +113,71 @@ class UserCreate(BaseModel):
 
 class UserOut(BaseModel):
     username: str
+    model_config = ConfigDict(from_attributes=True)
 
 
-def find_task_by_id(task_id: int) -> TasksOut | None:
-    for task in tasks_db:
-        if task.id == task_id:
-
-            return task
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="task not found")
+class Base(DeclarativeBase):
+    pass
 
 
-def filter_tasks(status: StatusVar | None = None, priority: PriorityVar | None = None):
-    result = []
-    if not status and not priority:
-        result = tasks_db
-    for item in tasks_db:
-        if item.status == status and priority is None:
-            result.append(item)
-        if item.priority == priority and status is None:
-            result.append(item)
-        if item.status == status and item.priority == priority:
-            result.append(item)
+class UserDB(Base):
 
-    return result
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(nullable=False)
+
+
+class TaskDB(Base):
+    __tablename__ = "tasks"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(nullable=False)
+    description: Mapped[str | None] = mapped_column(nullable=True)
+    priority: Mapped[PriorityVar] = mapped_column(nullable=False)
+    status: Mapped[StatusVar] = mapped_column(nullable=False)
+    created_at: Mapped[datetime] = mapped_column(nullable=False)
+
+
+Base.metadata.create_all(engine)
+
+
+def find_task_by_id(task_id: int, db: Annotated[Session, Depends(get_db)]) -> TaskDB:
+    task = db.scalar(select(TaskDB).where(TaskDB.id == task_id))
+    if task is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="task not found"
+        )
+    return task
+
+
+def filter_tasks(
+    db: Session,
+    status: StatusVar | None = None,
+    priority: PriorityVar | None = None,
+) -> list[TaskDB]:
+    stmt = select(TaskDB)
+    if status is not None:
+        stmt = stmt.where(TaskDB.status == status)
+    if priority is not None:
+        stmt = stmt.where(TaskDB.priority == priority)
+
+    return db.scalars(stmt).all()
 
 
 def order_reverse(order):
     return order == SortOrder.desc
 
 
-def get_user(username):
-    user = users_db.get(username)
+def get_user(username: str, db: Session):
+    user = db.scalar(select(UserDB).where(UserDB.username == username))
     return user
 
 
-def authenticate_user(username, password):
-    user = get_user(username)
+def authenticate_user(username, password, db: Session):
+    user = get_user(username, db)
     if not user:
         return None
-    correct_password = password_hash.verify(password, user["hashed_password"])
+    correct_password = password_hash.verify(password, user.hashed_password)
     if not correct_password:
         return None
 
@@ -159,7 +192,10 @@ def create_token(username):
     return token
 
 
-def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+def get_current_user(
+    token: Annotated[str, Depends(oauth2_scheme)],
+    db: Annotated[Session, Depends(get_db)],
+):
 
     credentiel_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
@@ -175,10 +211,10 @@ def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
             raise credentiel_exception
     except InvalidTokenError:
         raise credentiel_exception
-    user = get_user(username)
+    user = get_user(username, db)
     if user is None:
         raise credentiel_exception
-    return User(username=user["username"])
+    return user.username
 
 
 @app.get("/health", status_code=status.HTTP_200_OK)
@@ -188,21 +224,25 @@ async def get_health():
 
 @app.post("/tasks", status_code=201)
 async def create_tasks(
-    current_user: Annotated[User, Depends(get_current_user)], task_in: TasksIn
+    current_user: Annotated[str, Depends(get_current_user)],
+    db: Annotated[Session, Depends(get_db)],
+    task_in: TasksIn,
 ) -> TasksOut:
-    task = TasksOut(
+    task = TaskDB(
         **task_in.model_dump(),
-        id=next_id(),
+        status=StatusVar.todo,
         created_at=datetime.now(timezone.utc),
-        status="todo"
     )
-    tasks_db.append(task)
+    db.add(task)
+    db.commit()
+    db.refresh(task)
     return task
 
 
 @app.get("/tasks")
 async def get_tasks(
-    current_user: Annotated[User, Depends(get_current_user)],
+    current_user: Annotated[str, Depends(get_current_user)],
+    db: Annotated[Session, Depends(get_db)],
     status: StatusVar | None = None,
     priority: PriorityVar | None = None,
     page: int = Query(1, ge=1),
@@ -211,7 +251,7 @@ async def get_tasks(
     order: SortOrder = Query(SortOrder.asc),
 ) -> PageSortTask:
 
-    filtered_tasks = filter_tasks(status, priority)
+    filtered_tasks = filter_tasks(db, status, priority)
     priority_rank = {"low": 1, "medium": 2, "high": 3}
     status_rank = {"todo": 1, "in_progress": 2, "done": 3}
 
@@ -255,60 +295,71 @@ async def get_tasks(
 
 @app.get("/tasks/{task_id}", status_code=status.HTTP_200_OK)
 async def get_tasks_by_id(
-    current_user: Annotated[User, Depends(get_current_user)],
-    task: Annotated[TasksOut, Depends(find_task_by_id)],
-):
+    current_user: Annotated[str, Depends(get_current_user)],
+    task: Annotated[TaskDB, Depends(find_task_by_id)],
+) -> TasksOut:
 
     return task
 
 
 @app.patch("/tasks/{task_id}", status_code=status.HTTP_200_OK)
 async def update_tasks(
-    task: Annotated[TasksOut, Depends(find_task_by_id)],
+    task: Annotated[TaskDB, Depends(find_task_by_id)],
     data: UpdateData,
-    current_user: Annotated[User, Depends(get_current_user)],
-):
-    stored_data = task.model_dump()
-    updated_data = data.model_dump(exclude_unset=True)
+    current_user: Annotated[str, Depends(get_current_user)],
+    db: Annotated[Session, Depends(get_db)],
+) -> TasksOut:
+    update = data.model_dump(exclude_unset=True)
+    for key, value in update.items():
+        setattr(task, key, value)
 
-    for key in updated_data:
-        if updated_data[key] != stored_data[key]:
-            setattr(task, key, updated_data[key])
+    db.commit()
+    db.refresh(task)
     return task
 
 
 @app.delete("/tasks/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_task(
-    current_user: Annotated[User, Depends(get_current_user)],
-    task: Annotated[TasksOut, Depends(find_task_by_id)],
+    current_user: Annotated[str, Depends(get_current_user)],
+    task: Annotated[TaskDB, Depends(find_task_by_id)],
+    db: Annotated[Session, Depends(get_db)],
 ):
-    tasks_db.remove(task)
+    db.delete(task)
+    db.commit()
 
 
 @app.post("/token", response_model=Token)
-async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+async def login(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    db: Annotated[Session, Depends(get_db)],
+):
 
-    user = authenticate_user(form_data.username, form_data.password)
+    user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail=("password or username are incorrect"),
         )
 
-    access_token = create_token(user["username"])
+    access_token = create_token(user.username)
     return {"access_token": access_token, "token_type": "bearer"}
 
 
 @app.post("/register", status_code=status.HTTP_201_CREATED)
-async def registre(form_data: UserCreate) -> UserOut:
-    user = get_user(form_data.username)
+async def registre(
+    form_data: UserCreate, db: Annotated[Session, Depends(get_db)]
+) -> UserOut:
+    user = get_user(form_data.username, db)
     if user:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT, detail="username already registered"
         )
-    users_db[form_data.username] = {
-        "username": form_data.username,
-        "hashed_password": password_hash.hash(form_data.password),
-    }
+    user = UserDB(
+        username=form_data.username,
+        hashed_password=password_hash.hash(form_data.password),
+    )
 
-    return {"username": form_data.username}
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
